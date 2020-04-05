00ce6d89920710924a87a594284b96d4b00c55bf
Kevin Vitale <kevinvitale@gmail.com>
2020-04-05 16:50:02 -0400

    [BIP32] adds documentation for `KeyDepth`.

--- Sources/BIP32/KeyDepth.swift
+++ Sources/BIP32/KeyDepth.swift
@@ -1,15 +1,30 @@
 @propertyWrapper
+/**
+ * A wrapper around a `UInt8` number (between 0...255).
+ */
 public struct KeyDepth {
     public enum Error: Swift.Error {
+        /// Thrown when `nextDepth` is advanced past `UInt8.max`.
         case invalidDepth(UInt)
     }
     
+    /**
+     * - parameter depth:
+     */
     public init(wrappedValue depth: UInt8) {
         self.wrappedValue = depth
     }
     
+    /// The underlying `depth` value.
     public var wrappedValue: UInt8
     
+    /**
+     * Increments this `KeyDepth` by one.
+     *
+     * - warning: If the resulting value is equal to, or greater, than what an
+     *            unsigned 8-bit number can how (255), then this function throws
+     *            an `invalidDepth` error.
+     */
     public func nextDepth() throws -> KeyDepth {
         guard wrappedValue < .max else {
             throw Error.invalidDepth(UInt(wrappedValue).advanced(by: 1))
