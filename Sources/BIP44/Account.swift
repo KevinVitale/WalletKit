9b96925e9ee8dab2d7262903752eb2d5dab21c88
Kevin Vitale <kevinvitale@gmail.com>
2020-04-12 08:12:04 -0400

    [BIP44] extends Account w/default implementations

--- Sources/BIP44/Account.swift
+++ Sources/BIP44/Account.swift
@@ -1,21 +1,45 @@
 import Foundation
 import BIP32
 
-public protocol Account: CustomStringConvertible, Collection where Element == Self, Index == KeyIndex {
+public protocol Account: LazyCollectionProtocol, CustomStringConvertible where Element == Self, Index == KeyIndex {
     associatedtype Coin: CoinType
     
-    var pathURL :URL         { get }
-    var rootKey :ExtendedKey { get }
+    var pathURL    :URL         { get }
+    var rootKey    :ExtendedKey { get }
+    var isExternal :Bool        { get }
     
-    init(rootKey: ExtendedKey) throws
+    init(rootKey: ExtendedKey, isExternal: Bool) throws
 }
 
 extension Account {
-    public var privateKey: String {
-        Coin.privateKey(from: self.rootKey)
+    public var description: String {
+        self.rootKey.description
     }
     
-    public var publicKey: String {
-        try! Coin.publicKey(from: self.rootKey)
+    public var address: String {
+        try! Coin.address(from: self.rootKey)
+    }
+    
+    public var pathURL: URL {
+        self.rootKey.pathURL
+    }
+}
+
+extension Account {
+    public var startIndex : Index { .min }
+    public var endIndex   : Index { .max }
+    
+    public func index(after i: Index) -> Index {
+        i.index(after: i)
+    }
+    
+    public subscript(bounds: Range<KeyIndex>) -> Slice<Self> {
+        Slice<Self>.init(base: self, bounds: bounds)
+    }
+    
+    public subscript(position: Index) -> Self {
+        try! Self.init(rootKey: try! rootKey
+            .privateKey(atIndex: isExternal ? .normal(0) : .normal(1))
+            .privateKey(atIndex: position), isExternal: self.isExternal)
     }
 }
