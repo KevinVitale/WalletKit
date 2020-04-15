60ba40b620af757003aeed589621728d3cf5fc00
Kevin Vitale <kevinvitale@gmail.com>
2020-04-15 14:40:57 -0400

    [CLI] adds 'log(account:)' func

--- Sources/CLI/main.swift
+++ Sources/CLI/main.swift
@@ -7,19 +7,17 @@ struct CLI: ParsableCommand {
     
     @Option(name: .shortAndLong, help: "An optional passphrase for improving entropy.")
     var passphrase: String?
-
+    
     func run() throws {
         let wallet     = try Mnemonic(seedPhrase: seedPhrase).wallet(passphrase: passphrase ?? "")
         let account    = try wallet.account(coinType: .ETH, atIndex: 0)
         
-        account[.normal(0..<10)].forEach { account in
-            print(account.privateKey.pathURL, account.address)
-        }
-        
-        print()
-        account[.hardened(0..<10)].forEach { account in
-            print(account.privateKey.pathURL, account.address)
-        }
+        account[.normal(0..<10)].forEach { log(account: $0) }
+        account[.hardened(0..<10)].forEach { log(account: $0) }
+    }
+    
+    private func log<Account: AccountProtocol>(account: Account) {
+        print(account.privateKey.pathURL, account.address, "0x" + account.privateKey.key.dropFirst().hexString)
     }
 }
 
